4de4e2c413b85b80b8db79889565e8af3001e34f
Shubham Agarwal <shubhamsiinghal@gmail.com>
2018-06-04 16:48:34 +0530

    motion detection in video

---
+++ motion_detection.py
@@ -0,0 +1,17 @@
+#!//usr/bin/python
+
+import cv2,numpy
+
+cap=cv2.VideoCapture(0)
+
+while cap.isOpened():
+	
+	status,frame=cap.read()
+	# inRange() function to detect only green color
+	only_green=cv2.inRange(frame,(0,0,0),(10,10,10))
+	cv2.imshow("detect",only_green)
+	if cv2.waitKey(1) & 0xFF==ord('q'):
+		break
+
+cv2.destroyAllWindows()
+cap.release()
