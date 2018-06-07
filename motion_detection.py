ed9345c89f2d4d8ac0a69483a07e3b413352d0fd
Shubham Agarwal <shubhamsiinghal@gmail.com>
2018-06-07 20:16:55 +0530

    Motion Tracking

--- motion_detection.py
+++ motion_detection.py
@@ -2,13 +2,13 @@
 
 import cv2,numpy
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(1)
 
 while cap.isOpened():
 	
 	status,frame=cap.read()
 	# inRange() function to detect only green color
-	only_green=cv2.inRange(frame,(0,0,0),(10,10,10))
+	only_green=cv2.inRange(frame,(0,0,40),(10,10,255))
 	cv2.imshow("detect",only_green)
 	if cv2.waitKey(1) & 0xFF==ord('q'):
 		break
---
+++ motion_tracker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# import cv2 and numpy library
+
+import cv2,numpy,os
+
+#	Function to create differences
+def create_diff(x,y,z):
+	diff1=cv2.absdiff(x,y)
+	diff2=cv2.absdiff(y,z)
+	diff3=cv2.bitwise_and(diff1,diff2)
+	return diff3
+
+#	To turn on the camera
+cap = cv2.VideoCapture(1)
+
+#	take pictures
+
+img1=cap.read()[1]
+img2=cap.read()[1]
+img3=cap.read()[1]
+
+#	Conversion of images into grayscale mode
+
+gray1=cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2GRAY)
+gray2=cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2GRAY)
+gray3=cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2GRAY)
+
+while (1):
+
+	new_img=create_diff(gray1,gray2,gray3)
+	cv2.imshow("Motion",new_img)
+
+	temp_img=cap.read()[1]
+	cv2.imshow("Original",temp_img)
+
+	#	Shifting of images to contionuos detecting
+	gray1=gray2
+	gray2=gray3
+	gray3=cv2.cvtColor(temp_img,cv2.COLOR_BGR2GRAY)
+
+	if cv2.waitKey(1) & 0xFF==ord('q') :
+		break
+
+cv2.destroyAllWindows()
+cap.release()
